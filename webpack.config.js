f4212805a70fbe92b58efabf27fab11e26287e8d
Brian Embry <b.a.embry@gmail.com>
2019-07-03 19:34:30 -0500

    First commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,37 @@
-const path = require('path');
-const SRC_DIR = path.join(__dirname, '/client');
-const DIST_DIR = path.join(__dirname, '/dist');
+const path = require("path");
+const SRC_DIR = path.join(__dirname, "/client");
+const DIST_DIR = path.join(__dirname, "/dist");
+// module.exports = {
+//   entry: `${SRC_DIR}/index.jsx`,
+//   output: {
+//     filename: "bundle.js",
+//     path: DIST_DIR
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         test: /\.jsx?/,
+//         include: SRC_DIR,
+//         loader: "babel-loader",
+//         query: {
+//           presets: ["react", "es2015"]
+//         }
+//       }
+//     ]
+//   }
+// };
 module.exports = {
   entry: `${SRC_DIR}/index.js`,
   output: {
-  filename: 'bundle.js',
-  path: DIST_DIR
-},
-  module : {
-    rules : [
+    filename: "bundle.js",
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
       {
-        test : /\.js?/,
-        include : SRC_DIR,
-        loader : 'babel-loader',
+        test: /\.js?/,
+        include: SRC_DIR,
+        loader: "babel-loader"
       }
     ]
   }
